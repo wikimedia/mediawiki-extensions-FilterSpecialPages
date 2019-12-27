34e8bd79eea20ba6ef631c48a60b06536908afc8
Zoranzoki21 <zorandori4444@gmail.com>
2019-12-27 03:30:16 +0100

    Add phpcs and make it pass
    Change-Id: Ifcb72f952d87971f703ca9ee209d8a6f99823331

--- FilterSpecialPages.php
+++ FilterSpecialPages.php
@@ -2,11 +2,16 @@
 
 class FilterSpecialPages {
 
-	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
-		if( $out->getTitle()->isSpecial( "Specialpages") ){
-			$out->addModules("ext.filterspecialpages");
+	/**
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/BeforePageDisplay
+	 *
+	 * @param OutputPage $out
+	 * @param Skin $skin
+	 */
+	public static function onBeforePageDisplay( OutputPage $out, $skin ) {
+		if ( $out->getTitle()->isSpecial( 'Specialpages' ) ) {
+			$out->addModules( 'ext.filterspecialpages' );
 		}
-		return true;
 	}
 
 }
