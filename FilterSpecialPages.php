6ba3b345a4864917ba9f6bf07e002a06f592a2ac
Hua Jing <hua.jing@hallowelt.com>
2024-10-15 11:21:32 +0200

    Hide special pages from Special:SpecialPages
    Add filter component to the overwritten version of Special:SpecialPages
    
    ERM38624
    
    [4.5.x]
    
    WCAG
    
    Change-Id: Ibb260e98280f387cb1b407e90e575c3c4022747c

--- FilterSpecialPages.php
+++ FilterSpecialPages.php
@@ -9,7 +9,7 @@ class FilterSpecialPages {
 	 * @param Skin $skin
 	 */
 	public static function onBeforePageDisplay( OutputPage $out, $skin ) {
-		if ( $out->getTitle()->isSpecial( 'Specialpages' ) ) {
+		if ( $out->getTitle()->isSpecial( 'Specialpages' ) || $out->getTitle()->isSpecial( 'EnhancedSpecialPages' ) ) {
 			$out->addModules( 'ext.filterspecialpages' );
 		}
 	}
